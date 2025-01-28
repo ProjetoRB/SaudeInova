f19dcf0eaecf6ecb9848d8bd325acf0fb4d7304c
vitordsb <vitordsb2019@gmail.com>
2025-01-28 01:10:16 -0300

    new page Login not completed yet

---
+++ src/app/cadastro/page.jsx
@@ -0,0 +1,24 @@
+"use client"
+
+import styled from "styled-components"
+
+export const Button = styled.button`
+    color: #ffffff;
+    background-color: #00b7ff97;
+    padding: 20px;
+    border-radius: 10px;
+    border: none;
+    cursor: pointer;
+`
+
+
+const Cadastro = () => {
+    return (
+        <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non maxime, amet cum placeat exercitationem laborum quasi numquam voluptatem odio at expedita, necessitatibus iure blanditiis consequuntur. Ducimus suscipit qui quam cupiditate.
+            <Button>Login</Button>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/app/page.jsx
@@ -0,0 +1,20 @@
+"use client"
+import styles from "./Login.module.css"
+
+const Login = () => {
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <div className={styles.leftside}>
+          
+        </div>
+        <div className={styles.rightside}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus, quis, voluptatibus odio error dolore reiciendis nesciunt possimus expedita, tempora omnis nostrum aliquam corrupti accusamus vitae veniam voluptatem officia numquam quos.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login
