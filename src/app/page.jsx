89c83694d9c628b8eb6f74c4ce327f6072319829
kaique04 <kaiquedossantosmoreira1@gmail.com>
2025-02-01 23:48:52 -0300

    ajustes no login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,17 +8,23 @@ const Login = () => {
     <div className={styles.container}>
       <div className={styles.box}>
         <div className={styles.leftside}>
-          <Image className={styles.logo} src="/LogoSemFundo.png" alt="Logo" width={400} height={400} />
+          <Image className={styles.logo} src="/LogoSemFundo.png" alt="Logo" width={300} height={300} />
           <p>Cadastre-se agora e receba as melhores ofertas de serviço médico do país!</p>
         </div>
         <div className={styles.rightside}>
-          <h1 className={styles.title}>vitor gay</h1>
+          <h1 className={styles.title}>Entrar</h1>
           <form action="" className={styles.form}>
-            <input type="email" placeholder="Email" className={styles.input} />
-            <input type="password" placeholder="Senha" className={styles.input} />
+            <div className={styles.email}>
+              <input type="email" placeholder="Email" className={styles.input} />
+              <i className="bi bi-envelope"></i>
+            </div>
+            <div className={styles.senha}>
+              <input type="password" placeholder="Senha" className={styles.input} />
+              <i className="bi bi-eye"></i>
+            </div>
             <button className={styles.button}>Entrar</button>
             <div className={styles.links}>
-              <a href="#">Esqueceu a senha? clique aqui</a>
+              <a href="#">Esqueceu a senha?</a>
               <a href="#">Cadastre-se</a>
             </div>
           </form>
